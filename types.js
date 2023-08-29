574d1faad66abcc9128b09dccd4d0118239d3dd4
piasahmed1 <ahmedpias229@gmail.com>
2023-08-29 23:03:16 +0600

    first commit

---
+++ types.js
@@ -0,0 +1,32 @@
+
+// strongly typed language
+// int a = 5;
+// string b = 'alim halim khaliam';
+// bool c = true;
+// object student = {name: 'noya dhman', id: 55}
+// int [] numbers = [12, 45, 78]
+// string [] friends = ['abul', 'babul',];
+
+
+// javaScript is a dynamic type language
+const a = 5;
+const b = 'samsu  akon ar copai na';
+const d = true;
+
+// non primitive type
+const ages = [45, 65, 48,];
+const student = {id: 23, class: 7}
+// console.log(typeof a, typeof b, typeof ages, typeof student);
+
+let x = 5;
+let y = x;
+
+console.log(x, y);
+y = 7;
+
+console.log(x, y)
+
+
+let = p = {job: 'web developer'};
+let q = p;
+console.log(p, q)
